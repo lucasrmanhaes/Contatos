4a3ad9836064961328b083803ff218c8f9ffa200
lucas <ramosmanhaes@outlook.com>
2024-05-06 13:36:30 -0300

    Criando a interface de persistencia

--- src/main/java/br/com/lucas/contatos/model/Contato.java
+++ src/main/java/br/com/lucas/contatos/model/Contato.java
@@ -1,12 +1,12 @@
 package br.com.lucas.contatos.model;
 
 import jakarta.persistence.*;
-
+import lombok.Data;
 import java.time.LocalDate;
 import java.util.Objects;
-import java.util.UUID;
 
 @Entity(name = "TB_CONTATO")
+@Data
 public class Contato {
 
     @Id
@@ -18,37 +18,6 @@ public class Contato {
     private String email;
     @Column(name = "data_nascimento")
     private LocalDate dataNascimento;
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getTelefone() {
-        return telefone;
-    }
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public LocalDate getDataNascimento() {
-        return dataNascimento;
-    }
-    public void setDataNascimento(LocalDate dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
 
     @Override
     public boolean equals(Object o) {
--- src/main/java/br/com/lucas/contatos/repository/ContatoRepository.java
+++ src/main/java/br/com/lucas/contatos/repository/ContatoRepository.java
@@ -1,4 +1,12 @@
 package br.com.lucas.contatos.repository;
 
-public class ContatoRepository {
-}
+import br.com.lucas.contatos.model.Contato;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.util.List;
+
+    public interface ContatoRepository extends JpaRepository<Contato, Long> {
+        List<Contato> findByNomeContaining(String nome);
+        List<Contato> findByDataNascimentoBetween(LocalDate startDate, LocalDate endDate);
+    }
+
